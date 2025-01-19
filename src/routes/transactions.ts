96d89947f97c3c6ef09d3cc24f59adc3153f1a0e
JoseM4rcio <jm4536@outlook.com>
2025-01-19 17:08:03 -0300

    Criação de transações

---
+++ src/routes/transactions.ts
@@ -0,0 +1,29 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { randomUUID } from "node:crypto";
+import { knex } from "../database";
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post("/", async (request, response) => {
+    request.body;
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body
+    );
+
+    await knex("transactions")
+      .insert({
+        id: randomUUID(),
+        title,
+        amount: type === "credit" ? amount : amount * -1,
+      })
+      .returning("*");
+
+    return response.status(201).send();
+  });
+}
