36887a66535438fd0e5bd06fa420b78ad35954b2
JoseM4rcio <jm4536@outlook.com>
2025-01-19 20:58:28 -0300

    Resumo de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -26,6 +26,16 @@ export async function transactionsRoutes(app: FastifyInstance) {
     };
   });
 
+  app.get("/summary", async () => {
+    const summary = await knex("transactions")
+      .sum("amount", { as: "amount:" })
+      .first();
+
+    return {
+      summary,
+    };
+  });
+
   app.post("/", async (request, response) => {
     request.body;
     const createTransactionBodySchema = z.object({
